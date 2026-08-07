7079aad069efbd78c815cf1c18ebfb57fd0f59e2
sleepysdevin <sleepysdev@proton.me>
2026-08-07 07:13:34 -0500

    Add canonical public contact link

--- links/runtime.js
+++ links/runtime.js
@@ -3,6 +3,7 @@ export const brandLinks = Object.freeze({
   sponsors: 'https://github.com/sponsors/sleepy-studio',
   x: 'https://x.com/TheSleepyStudio',
   website: 'https://sleepystudio.xyz',
+  email: 'mailto:Contact@SleepyStudio.xyz',
 })
 
 export const brandLinkIds = Object.freeze(Object.keys(brandLinks))
