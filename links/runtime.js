db6050292b65132a47ef6120fe46a374d578e20b
sleepysdevin <sleepysdev@proton.me>
2026-08-07 07:10:42 -0500

    Add canonical public Brand destinations

---
+++ links/runtime.js
@@ -0,0 +1,14 @@
+export const brandLinks = Object.freeze({
+  github: 'https://github.com/Sleepy-Studio',
+  sponsors: 'https://github.com/sponsors/sleepy-studio',
+  x: 'https://x.com/TheSleepyStudio',
+  website: 'https://sleepystudio.xyz',
+})
+
+export const brandLinkIds = Object.freeze(Object.keys(brandLinks))
+
+export function getBrandLink(id) {
+  const href = brandLinks[id]
+  if (!href) throw new Error(`Unknown Brand link: ${id}`)
+  return href
+}
