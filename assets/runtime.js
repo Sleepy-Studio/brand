5098e39cf3cc7fcc164716b746bd3df39a56fe4a
sleepysdevin <sleepysdev@proton.me>
2026-08-09 00:50:51 -0500

    feat: expose canonical runtime asset catalog

--- assets/runtime.js
+++ assets/runtime.js
@@ -8,6 +8,65 @@ export const brandAssetUrls = Object.freeze({
   favicon: new URL('../favicon.ico', import.meta.url).href,
 })
 
+export const brandAssetCatalog = Object.freeze([
+  Object.freeze({
+    id: 'logo-black',
+    name: 'Logo Black',
+    kind: 'logo',
+    variants: Object.freeze([
+      Object.freeze({ format: 'svg', file: 'LogoBlack.svg', mime: 'image/svg+xml', url: brandAssetUrls['logo-black'] }),
+    ]),
+  }),
+  Object.freeze({
+    id: 'logo-black-red',
+    name: 'Logo Black Red',
+    kind: 'logo',
+    variants: Object.freeze([
+      Object.freeze({ format: 'svg', file: 'LogoBlackRed.svg', mime: 'image/svg+xml', url: brandAssetUrls['logo-black-red'] }),
+    ]),
+  }),
+  Object.freeze({
+    id: 'logo-white',
+    name: 'Logo White',
+    kind: 'logo',
+    variants: Object.freeze([
+      Object.freeze({ format: 'svg', file: 'LogoWhite.svg', mime: 'image/svg+xml', url: brandAssetUrls['logo-white'] }),
+    ]),
+  }),
+  Object.freeze({
+    id: 'logo-white-red',
+    name: 'Logo White Red',
+    kind: 'logo',
+    variants: Object.freeze([
+      Object.freeze({ format: 'svg', file: 'LogoWhiteRed.svg', mime: 'image/svg+xml', url: brandAssetUrls['logo-white-red'] }),
+    ]),
+  }),
+  Object.freeze({
+    id: 'logo-yellow',
+    name: 'Logo Yellow',
+    kind: 'logo',
+    variants: Object.freeze([
+      Object.freeze({ format: 'svg', file: 'LogoYellow.svg', mime: 'image/svg+xml', url: brandAssetUrls['logo-yellow'] }),
+    ]),
+  }),
+  Object.freeze({
+    id: 'logo-3d',
+    name: 'Logo 3D',
+    kind: 'logo',
+    variants: Object.freeze([
+      Object.freeze({ format: 'glb', file: 'SleepyLogo3d.glb', mime: 'model/gltf-binary', url: brandAssetUrls['logo-3d'] }),
+    ]),
+  }),
+  Object.freeze({
+    id: 'favicon',
+    name: 'Favicon',
+    kind: 'app-icon',
+    variants: Object.freeze([
+      Object.freeze({ format: 'ico', file: 'favicon.ico', mime: 'image/x-icon', url: brandAssetUrls.favicon }),
+    ]),
+  }),
+])
+
 export const brandAssetIds = Object.freeze(Object.keys(brandAssetUrls))
 
 export function getBrandAssetUrl(id) {
